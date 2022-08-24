a4adacb7117af67387385e7fefe9ecab1f6506a9
AH050 <111998807+AH050@users.noreply.github.com>
2022-08-24 11:26:40 -0400

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -10,8 +10,8 @@ public class Hello {
   public static void main(String[] args)  {
   
     System.out.println("<Adeline John>");
-	System.out.println("<Hello java>");
-	System.out.println("<Introduction of myself>");
+	System.out.println("<Hello World>");
+	System.out.println("<Introduction>");
 	System.out.println("<8/24/2022>");
 	System.out.println("<CSMC 255>");
 	System.out.println();
